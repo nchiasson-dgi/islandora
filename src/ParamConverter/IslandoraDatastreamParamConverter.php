27d5dc0dc65014626f8dff0c2f11bd39ed9dfd80
Ryan Brinn <ryanbrinn@gmail.com>
2017-06-22 13:34:28 -0300

    Changes to be committed: 	new file:   src/Access/IslandoraDatastreamAccess.php 	new file:   src/ParamConverter/IslandoraDatastreamParamConverter.php

---
+++ src/ParamConverter/IslandoraDatastreamParamConverter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\islandora\ParamConverter;
+
+use Drupal\Core\ParamConverter\ParamConverterInterface;
+use Symfony\Component\Routing\Route;
+
+
+class IslandoraDatastreamParamConverter implements ParamConverterInterface {
+  public function convert($value, $definition, $name, array $defaults) {
+    // XXX: This seems so very dumb but given how empty slugs don't play nice
+    // in Drupal as defaults this needs to be the case. If it's possible to get
+    // around this by making the empty slug route in YAML or a custom Routing
+    // object we can remove this.
+    //$object = ?
+    return islandora_datastream_load($value, $defaults['object']->id);
+  }
+
+  public function applies($definition, $name, Route $route) {
+    return (!empty($definition['type']) && $definition['type'] == 'datastream');
+  }
+}
