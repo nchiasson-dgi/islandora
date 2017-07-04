487ca3fb9fe5094d64e50d8d7aef942a6631fe8d
Jordan Dukart <jordan@discoverygarden.ca>
2017-07-04 12:08:17 -0300

    Add multiple permission checking.

--- src/Access/IslandoraDatastreamAccess.php
+++ src/Access/IslandoraDatastreamAccess.php
@@ -6,10 +6,31 @@ use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
 
-
+/**
+ * Access checking for datastreams on objects within Islandora.
+ */
 class IslandoraDatastreamAccess implements AccessInterface {
 
-  public function access($perms, $object, $datastream, AccountInterface $account) {
+  /**
+   * Whether the user has access to a datastream on an object.
+   *
+   * @param string|array $perms
+   *   A singular permission or an array of permissions to be evalulated.
+   * @param AbstractObject $object
+   *   A loaded Fedora object.
+   * @param AbstractDatastream $datastream
+   *   The loaded datastream being accessed.
+   * @param AccountInterface $account
+   *   User being validated against.
+   * @param string $islandora_access_conjunction
+   *   If an array of permissions is specified this will dictate how it's
+   *   evaluated. To maintain 7's behavior these are ORed together by default
+   *   but can be overridden on a per route basis.
+   *
+   * @return AccessResult|\Drupal\Core\Access\AccessResultAllowed|\Drupal\Core\Access\AccessResultForbidden|\Drupal\Core\Access\AccessResultNeutral
+   *   Whether the user has access in AccessResult object form.
+   */
+  public function access($perms, AbstractObject $object, AbstractDatastream $datastream, AccountInterface $account, $islandora_access_conjunction = 'OR') {
     module_load_include('inc', 'islandora', 'includes/utilities');
     // XXX: This seems so very dumb but given how empty slugs don't play nice
     // in Drupal as defaults this needs to be the case. If it's possible to get
@@ -18,9 +39,18 @@ class IslandoraDatastreamAccess implements AccessInterface {
     $datastream = islandora_datastream_load($datastream, $object);
     if (!$datastream && !islandora_describe_repository()) {
       islandora_display_repository_inaccessible_message();
-      return FALSE;
+      return AccessResult::forbidden();
     }
-    return AccessResult::allowedIf(islandora_datastream_access($perms, $datastream, $account));
+    if (is_array($perms)) {
+      $result = AccessResult::neutral();
+      foreach ($perms as $perm) {
+        $result = $islandora_access_conjunction == 'AND' ? $result->andIf(AccessResult::allowedIf(islandora_datastream_access($perm, $object, $account))) : $result->orIf(AccessResult::allowedIf(islandora_datastream_access($perm, $object, $account)));
+      }
+      return $result;
+    }
+    else {
+      return AccessResult::allowedIf(islandora_datastream_access($perms, $object, $account));
+    }
   }
 
 }
