bd1006667aebd70015aa36f2aef5c60b6ef73eb8
willtp87 <willtp87@hotmail.com>
2017-09-10 15:47:45 -0300

    Some form state updates

--- src/Form/IslandoraIngestForm.php
+++ src/Form/IslandoraIngestForm.php
@@ -20,10 +20,44 @@ class IslandoraIngestForm extends FormBase {
   }
 
   /**
-   * {@inheritdoc}
+   * Ingest form build function.
+   *
+   * Initializes the form state, and builds the initial list of steps, excutes
+   * the current step.
+   *
+   * @param array $form
+   *   The Drupal form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The Drupal form state.
+   * @param array $configuration
+   *   An associative array of configuration values that are used to build the
+   *   list of steps to be executed, including:
+   *   - id: The PID with which the object should be created.
+   *   - namespace: The PID namespace in which the object should be created.
+   *     (id is used first, if it is given).
+   *   - label: The initial label for the object. Defaults to "New Object".
+   *   - collections: An array of collection PIDs, to which the new object should
+   *     be related.
+   *   - models: An array of content model PIDs, to which the new object might
+   *     subscribe
+   *   - parent: The parent of the child to be ingested. This is needed for XACML
+   *     to correctly apply the parent's POLICY to children.
+   *
+   * @return array
+   *   The form definition of the current step.
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    return $form;
+  public function buildForm(array $form, FormStateInterface $form_state, $configuration = NULL) {
+    module_load_include('inc', 'islandora', 'includes/ingest.form');
+    try {
+      islandora_ingest_form_init_form_state_storage($form_state, $configuration);
+      return islandora_ingest_form_execute_step($form, $form_state);
+    }
+    catch (Exception $e) {
+      \Drupal::logger('islandora')->error('Exception during ingest form processing with Message: "@exception",  and Trace: @trace', array('@exception' => $e->getMessage(), '@trace' => $e->getTraceAsString()));
+      drupal_set_message($e->getMessage(), 'error');
+      return array(array(
+          '#markup' => \Drupal::l(t('Back'), \Drupal\Core\Url::fromUri('javascript:window.history.back();'))));
+    }
   }
 
   /**
