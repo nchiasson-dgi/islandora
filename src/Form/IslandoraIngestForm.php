5f287251e93a66554eb028b6f17106f9d1574cbf
willtp87 <willtp87@hotmail.com>
2017-09-12 16:22:16 -0300

    lets keep the submit where it is for now and fix the spinner lib

--- src/Form/IslandoraIngestForm.php
+++ src/Form/IslandoraIngestForm.php
@@ -72,41 +72,7 @@ class IslandoraIngestForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     module_load_include('inc', 'islandora', 'includes/ingest.form');
-    // Execute any remaining callbacks.
-    islandora_ingest_form_increment_step($form_state);
-    $step = islandora_ingest_form_get_step($form_state);
-    if (isset($step) && $step['type'] == 'callback') {
-      islandora_ingest_form_execute_consecutive_callback_steps($form, $form_state, $step);
-    }
-    // Ingest the objects.
-    $set_redirect = $form_state->getRedirect() ? FALSE : TRUE;
-    foreach ($form_state->get(['islandora', 'objects']) as &$object) {
-      try {
-        islandora_add_object($object);
-        // We want to redirect to the first object as it's considered to be the
-        // primary object.
-        if ($set_redirect) {
-          $form_state->setRedirect("islandora/object/{$object->id}");
-          $set_redirect = FALSE;
-        }
-        drupal_set_message(
-        $this->t('"@label" (ID: @pid) has been ingested.', array('@label' => $object->label, '@pid' => $object->id)),
-        'status');
-      }
-      catch (Exception $e) {
-        // If post hooks throws it may already exist at this point but may be
-        // invalid, so don't say failed.
-        \Drupal::logger('islandora')->error('Exception during ingest with Message: "@exception",  and Trace: @trace', array('@exception' => $e->getMessage(), '@trace' => $e->getTraceAsString()));
-        drupal_set_message(
-          $this->t('A problem occured while ingesting "@label" (ID: @pid), please notify the administrator.',
-          array('@label' => $object->label, '@pid' => $object->id)),
-          'error'
-        );
-      }
-    }
-    // XXX: Foreaching with references can be weird... The reference exists in
-    // the scope outside.
-    unset($object);
+    islandora_ingest_form_submit($form, $form_state);
   }
 
 }
