2b601ee4e6fdae623a2dfce4e88d0cc2d9458edf
Adam Vessey <adam-vessey@users.noreply.github.com>
2018-01-03 10:21:00 -0400

    Add abstract base block classes.

---
+++ src/Plugin/Block/AbstractFormBlockBase.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\islandora\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Abstract form block base.
+ */
+abstract class AbstractFormBlockBase extends BlockBase implements ContainerFactoryPluginInterface {
+
+  protected $formBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+
+}
