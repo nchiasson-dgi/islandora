a4a171acbfe5bea37bfec15e3418b28055b6506c
willtp87 <willtp87@hotmail.com>
2018-04-16 15:49:16 -0300

    Handle warning when object is locked down

--- src/ParamConverter/DatastreamParamConverter.php
+++ src/ParamConverter/DatastreamParamConverter.php
@@ -14,6 +14,14 @@ class DatastreamParamConverter implements ParamConverterInterface {
    * Datastream parameter converter method.
    */
   public function convert($value, $definition, $name, array $defaults) {
+    if ($defaults['object'] === FALSE) {
+      // Return for 404.
+      return NULL;
+    }
+    elseif ($defaults['object'] === NULL) {
+      // Let the access layer take care of it.
+      return FALSE;
+    }
     // XXX: This seems so very dumb but given how empty slugs don't play nice
     // in Drupal as defaults this needs to be the case. If it's possible to get
     // around this by making the empty slug route in YAML or a custom Routing
