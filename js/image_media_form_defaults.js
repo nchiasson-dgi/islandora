2a5190e9ed288d9518893555dc994e2adb1a75a4
Noel Chiasson <noel@discoverygarden.ca>
2021-04-14 09:35:28 -0300

    Adding hidden description field to media form

--- js/image_media_form_defaults.js
+++ js/image_media_form_defaults.js
@@ -1,6 +1,12 @@
 (function (Drupal, $) {
   var ImageName = $('input[id=edit-name-0-value]').val();
+  var ImageDescription = $('input[data-drupal-selector=object-description]').val();
   $(document).ajaxSuccess(function () {
-    $('input[data-drupal-selector=edit-field-media-image-0-alt]').val(ImageName);
+    if (ImageDescription) {
+      $('input[data-drupal-selector=edit-field-media-image-0-alt]').val(ImageDescription);
+    }
+    else {
+      $('input[data-drupal-selector=edit-field-media-image-0-alt]').val(ImageName);
+    }
   });
 })(Drupal, jQuery);
